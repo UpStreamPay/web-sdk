f6a7456ad31aca8133288c45823fb8b4bfdc7a72
wasseing <paul.wasson.fr@gmail.com>
2025-07-28 21:35:06 +0200

    feat: push to npm registry

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,14 @@
 import { injectScript, getWindowNameSpace } from './utils';
 import { SDKs } from './SDKs';
-import { PublicEnvironment, Widget } from './types';
-import * as HeadlessCheckoutModule from './generated/types';
+import { PublicEnvironment } from './types';
+import { createHeadlessCheckout } from './generated/types';
 
 export * from './types';
 export * from './generated/types';
 
-export async function loadHeadlessCheckout(
-  env: PublicEnvironment = 'production',
-): Promise<typeof HeadlessCheckoutModule> {
-  await injectScript(SDKs['headless'][env]);
-  return getWindowNameSpace<typeof HeadlessCheckoutModule>('headless');
-}
+export type HeadlessCheckoutModule = { createHeadlessCheckout: typeof createHeadlessCheckout };
 
-export async function loadWidgetV3(env: PublicEnvironment = 'production'): Promise<Widget> {
-  await injectScript(SDKs['widget'][env]);
-  return getWindowNameSpace('widget');
+export async function loadHeadlessCheckout(env: PublicEnvironment = 'production'): Promise<HeadlessCheckoutModule> {
+  await injectScript(SDKs['headless'][env]);
+  return getWindowNameSpace<{ createHeadlessCheckout: typeof createHeadlessCheckout }>('headless');
 }
